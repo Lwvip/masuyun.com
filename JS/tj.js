a2514c98649787b12b8d3fdf8d47b6b9a973e8b7
老巫 <54791310+Lwvip@users.noreply.github.com>
2020-05-17 07:44:19 +0800

    Update tj.js

--- JS/tj.js
+++ JS/tj.js
@@ -1,1 +1,8 @@
-
+document.writeln("Copyright © 2011-2020 MaSuYun.Com Rights Reserved.  ICP备案许可证号：<a target=\'_blank\' href=\'http://beian.miit.gov.cn/\'>豫ICP备17016480号-6</a>");
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2e6da918d5a49f58660153ca4ba2731b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
